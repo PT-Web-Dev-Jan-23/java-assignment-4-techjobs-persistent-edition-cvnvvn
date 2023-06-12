269a461fe1e0e9eb679c94a6d1d31ad02650085f
cvnvvn <canaan.merrick@gmail.com>
2023-06-11 19:02:36 -0500

    added queries

--- queries.sql
+++ queries.sql
@@ -1,7 +1,19 @@
 -- Part 1: Test it with SQL
+--# Field, Type, Null, Key, Default, Extra
+'id', 'int', 'NO', 'PRI', NULL, ''
+'employer', 'varchar(255)', 'YES', '', NULL, ''
+'name', 'varchar(255)', 'YES', '', NULL, ''
+'skills', 'varchar(255)', 'YES', '', NULL, ''
+'employer_id', 'int', 'YES', 'MUL', NULL, ''
 
 -- Part 2: Test it with SQL
+SELECT name FROM employer
+WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
+DROP TABLE job;
 
 -- Part 4: Test it with SQL
+SELECT * FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+ORDER BY name ASC;
